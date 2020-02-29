c02c4c1a38665964cad9e896df6d815d07a81707
yuichiro-cloud <ghjhbc1@gmail.com>
2020-02-29 12:52:28 +0900

    commit

---
+++ app/assets/javascripts/pfs-top.js
@@ -0,0 +1,71 @@
+$(function(){
+  var loading = `<div class="load" id="loading">
+  l o a d i n g
+  <div class="load__box"></div>
+  </div>`
+
+  var welcome = `<div class="main__welcome">
+  Welcome
+  </div>`
+
+  var portfolio = `<div class="main__pf">
+  Portfolio
+  </div>`
+
+  var creator = `<div class="main__creator">
+  creator
+  </div>`
+
+  var name = `<div class="main__name">
+  Yuichiro Ito
+  </div>`
+
+  window.onload = function(){
+    // ページ読み込み時に実行したい処理
+    $("#page-loading").append(loading);
+    }
+
+  function loaderremove(){    //loadingボタンを消す
+    $("#page-loading").remove();
+  }
+  setTimeout(loaderremove,2000);    //一定時間でloadingを消す
+
+  $('#page-loaded').delay(2000).queue(function(){
+    $(".main").append(welcome);
+    $(function(){
+      function welcomeremove(){       //appendしたwelcomeを消すためにdocument
+        $(".main__welcome").remove();     //welcomeをremove
+      }
+      setTimeout(welcomeremove,1100);     //一定時間でwelcomeを消す
+      $.dequeue($("#page-loaded")[0]);
+    })    //appendしたwelcomeを消すためにdocument end
+});       //welcomeを追加する関数のend
+
+  $('#page-loaded').delay(2000).queue(function(){
+    console.log("pf")
+    $(".main").append(portfolio);
+    $(".main__pf").delay(2500).animate(
+      {"top":"100px"
+    }
+    )
+    $.dequeue($("#page-loaded")[0]);
+  });       //portfolioを追加する関数のend
+
+  $("#page-loaded").delay(3000).queue(function(){   //creatorをappend
+    $(".main").append(creator);
+    $(".main__creator").delay(2500).animate(
+      {"top":"200px"}
+    )
+    $.dequeue($("#page-loaded")[0]);
+  });
+
+  $("#page-loaded").delay(3000).queue(function(){   //nameをappend
+    $(".main").append(name);
+    $(".main__name").delay(2500).animate(
+      {"top":"300px"
+    }
+    )
+    $.dequeue($("#page-loaded")[0]);
+  });
+
+});
