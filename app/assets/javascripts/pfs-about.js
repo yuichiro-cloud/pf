d116775c6d3a96c8e12d5d811130e2517065b7f1
yuichiro-cloud <ghjhbc1@gmail.com>
2020-03-01 16:10:38 +0900

    aboutのスクロールを実装

--- app/assets/javascripts/pfs-about.js
+++ app/assets/javascripts/pfs-about.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  var about = `<div class="about__title">
+  var about = `<div class="box__title">
   A b o u t
   </div>`
 
@@ -9,15 +9,34 @@ $(function(){
   $('#about').delay(3000).queue(function(){
     console.log("ab")
     $("#about").append(about);
-    $(".about__title").delay(1300).animate(
+    $(".box__title").delay(1300).animate(
       {"top":"-300px"}
       )
-      // $.dequeue($("#page-loaded")[0]);
+      $.dequeue($("#about")[0]);
     });       //aboutを追加する関数のend
+    
+    $("#about").delay(3000).queue(function(){
+      $(".box").css(
+        {"height":"800vh"}
+        )
+        console.log("aaaaaa");
 
-    $(window).scroll(function() {
- 
-      console.log($(this).scrollTop());
+        
+        
+        $(window).scroll(function() {
+          console.log("iii")
+        console.log($(this).scrollTop());
+        
+          // );
+          $(".box__contents").animate(
+            {"width":"90%"},(4000)
+            )
+            
+          })
+        });
+        // $(".box__contents").css({"opacity":"1"})
+      // $(".box__contents").css({"opacity":"1"})
    
-  })
+  // var scroll = $(window).scrollTop(188);
+  // console.log(scroll);
 });
