57471f1a229475669429e4612f2c5797d4eaa30e
yuichiro-cloud <ghjhbc1@gmail.com>
2020-03-02 03:24:37 +0900

    aboutの中身を作成

--- app/assets/javascripts/pfs-about.js
+++ app/assets/javascripts/pfs-about.js
@@ -7,9 +7,11 @@ $(function(){
 
   
   $('#about').delay(3000).queue(function(){
+    // $('#about').queue(function(){
     console.log("ab")
     $("#about").append(about);
     $(".box__title").delay(1300).animate(
+      // $(".box__title").animate(
       {"top":"-300px"}
       )
       $.dequeue($("#about")[0]);
@@ -23,13 +25,19 @@ $(function(){
 
         
         
-        $(window).scroll(function() {
+        $(window).scroll(function() {       //aboutが終わってからスクロールイベントが発火
           console.log("iii")
         console.log($(this).scrollTop());
         
           // );
           $(".box__contents").animate(
-            {"width":"90%"},(4000)
+            {"width":"90%"},(1000)
+            // {"width":"90%"}
+            )
+            $(".box__contents__text").animate(
+              {"fontSize":"20px"},(1000)
+              // {"fontSize":"20px"}
+              
             )
             
           })
