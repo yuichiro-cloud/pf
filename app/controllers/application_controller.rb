97c9f44bc7405cbebafcd78fea582d7cf7c2045d
yuichiro-cloud <ghjhbc1@gmail.com>
2020-03-09 14:10:32 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
-  private
+  # private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'admin' && password == '2222'
-    end
-  end
+  # def basic_auth
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == 'admin' && password == '2222'
+  #   end
+  # end
 end
